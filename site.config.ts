4e514ec6002668f3deeded90d0461ebef832d63a
tfahcry <125002016+tfahcry@users.noreply.github.com>
2023-04-16 16:51:19 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'b150df24a5134a80bee1caca7f5d8a44',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Teguh',
+  domain: 'some1ulov.eu.org',
+  author: 'Teguh',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
